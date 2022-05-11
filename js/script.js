e7fe52a04b18c987737c383adb556fc8223a4f71
Un gars <noellor@ndlp.fr>
2022-05-11 10:04:13 +0200

    a

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 function copy(classe) {
     text = document.getElementsByClassName(classe)[0].value;
-    navigator.clipboard.writeText(text);
+    try {
+        navigator.clipboard.writeText(text);
+    } catch (TypeError) {
+        alert("Un problème est survenu durant la copie.<br/>Veuillez copier le texte présent sur la ligne suivant<br/>"+text);
+    }
+    
 }
 function sendto(link) {
     window.open(
